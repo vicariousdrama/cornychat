19c7bb90de8fad5fa31b4e91c9deaa1aeed33933
Vic <vic@cornychat.com>
2025-02-12 05:56:47 -0500

    Fix MOTD
    - Expect MOTD to be a JSON structure, not a simple string. Parse accordingly.

--- pantry/routes/motdRouter.js
+++ pantry/routes/motdRouter.js
@@ -3,11 +3,17 @@ const {get} = require('../services/redis');
 const router = express.Router();
 
 router.get('', async function (req, res) {
-    let motdkey = `motd`;
-    let motdinfo = await get(motdkey);
-    let defaultMOTD = `Rooms that have not been accessed by anyone for 90 days will be deleted starting January 1, 2025`;
-    res.type('application/json');
-    res.send({motd:(motdinfo || defaultMOTD)});
+  let motdkey = `motd`;
+  let motdinfo = undefined;
+  try {
+    motdinfo = await get(motdkey);
+    motdinfo = motdinfo.motd;
+  } catch (e) {
+    motdinfo = undefined;
+  }
+  let defaultMOTD = `Rooms that have not been accessed by anyone for 90 days will be deleted starting January 1, 2025`;
+  res.type('application/json');
+  res.send({motd: motdinfo || defaultMOTD});
 });
 
 module.exports = router;
